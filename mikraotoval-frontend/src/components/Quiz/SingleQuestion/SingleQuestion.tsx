399238e4e231371d4065f371e16feb20d8e9b50e
NadavKrashin <krashin23@gmail.com>
2024-02-11 15:55:29 +0200

    feat(SingleQuestion.tsx, main.tsx): creating a simple question component that gets the data as props and handles the answer click and color feedback

---
+++ mikraotoval-frontend/src/components/Quiz/SingleQuestion/SingleQuestion.tsx
@@ -0,0 +1,120 @@
+import React, { FC, useMemo, useState } from "react";
+import {
+  Typography,
+  Box,
+  Container,
+  FormControlLabel,
+  Checkbox,
+  Button,
+} from "@mui/material";
+import { Option, useClasses } from "./styles";
+
+type QuestionOption = {
+  id: number;
+  text: string;
+  isCorrect: boolean;
+  color?: string;
+};
+
+type propsType = {
+  questionTitle: string;
+  questionOptions: QuestionOption[];
+  isMultipleChoice: boolean;
+};
+
+const SingleQuestion: FC<propsType> = ({
+  questionTitle,
+  questionOptions,
+  isMultipleChoice,
+}: propsType) => {
+  const [clickedOptionsIds, setClickedOptionsIds] = useState<
+    QuestionOption["id"][]
+  >([]);
+  const [isQuestionSubmitted, setIsQuestionSubmitted] =
+    useState<boolean>(false);
+
+  const getOptionColor = ({ isCorrect }: QuestionOption) => {
+    return isCorrect ? "green" : "red";
+  };
+
+  const optionsWithColors = useMemo(() => {
+    return !isQuestionSubmitted
+      ? questionOptions
+      : questionOptions.map((option) => ({
+          ...option,
+          color:
+            !clickedOptionsIds.includes(option.id) && !option.isCorrect
+              ? ""
+              : getOptionColor(option),
+        }));
+  }, [isQuestionSubmitted]);
+
+  const handleQuestionSubmit = (optionsIds?: QuestionOption["id"][]) => {
+    if (optionsIds) {
+      setClickedOptionsIds(optionsIds);
+    }
+
+    setIsQuestionSubmitted(true);
+  };
+
+  const handleOptionClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const clickedOptionId = +event.target.value;
+    setClickedOptionsIds(
+      event.target.checked
+        ? [...clickedOptionsIds, clickedOptionId]
+        : clickedOptionsIds.filter((id) => id !== clickedOptionId)
+    );
+  };
+
+  const classes = useClasses();
+  return (
+    <div>
+      <Container className={classes.container}>
+        <Typography variant="h4">{questionTitle}</Typography>
+        <Box className={classes.optionsBox}>
+          {optionsWithColors.map((option) => {
+            return !isMultipleChoice ? (
+              <Option
+                elevation={4}
+                key={option.id}
+                sx={{ backgroundColor: option.color ?? "" }}
+                onClick={() =>
+                  !clickedOptionsIds.length && handleQuestionSubmit([option.id])
+                }
+              >
+                {option.text}
+              </Option>
+            ) : (
+              <Option key={option.id} elevation={4}>
+                <FormControlLabel
+                  sx={{
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: option.color ?? "",
+                  }}
+                  control={
+                    <Checkbox
+                      sx={{ marginLeft: "2vw" }}
+                      onChange={handleOptionClick}
+                      value={option.id}
+                    />
+                  }
+                  label={option.text}
+                />
+              </Option>
+            );
+          })}
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+          <Button>הבא</Button>
+          {isMultipleChoice && (
+            <Button onClick={() => handleQuestionSubmit()}>בדוק</Button>
+          )}
+          <Button>חזור</Button>
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default SingleQuestion;
--- mikraotoval-frontend/src/main.tsx
+++ mikraotoval-frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import RtlProvider from "./contexts/RtlProvider";
-import { Typography } from "@mui/material";
+import SingleQuestion from "./components/Quiz/SingleQuestion/SingleQuestion";
 
 const theme = createTheme({
   direction: "rtl",
@@ -13,7 +13,28 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <RtlProvider>
       <ThemeProvider theme={theme}>
         <main>
-          <Typography variant="h1">מקראות תובל</Typography>
+          <SingleQuestion
+            questionTitle="מי היה ראש הממשלה הראשון"
+            questionOptions={[
+              { id: 1, text: "בן גוריון", isCorrect: true },
+              {
+                id: 2,
+                text: "לוי אשכול",
+                isCorrect: false,
+              },
+              {
+                id: 3,
+                text: "גולדה מאיר",
+                isCorrect: false,
+              },
+              {
+                id: 4,
+                text: "יצחק רבין",
+                isCorrect: false,
+              },
+            ]}
+            isMultipleChoice={false}
+          />
         </main>
       </ThemeProvider>
     </RtlProvider>
